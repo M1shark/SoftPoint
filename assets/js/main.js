0a8cdb9bc683a1a6fce80e4145882221f4de8037
Михаил <88690428+M1shark@users.noreply.github.com>
2021-11-26 03:30:25 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,14 +3,3 @@ $(document).ready(function() {
         $('.header-navbar').toggleClass('active');
     });
 });
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
-    duration: 1800,
-    delay: 400,
-})
-sr.reveal('.hero-info,.software-card,.discover-info,.serving-img', { origin: 'left' })
-sr.reveal('.hero-img,.software-info,.discover-img,.serving-info', { origin: 'right' })
-sr.reveal('.claints-content', { origin: 'right' })
-sr.reveal('.get-info,.plan-content', { origin: 'top' })
-sr.reveal('.get-img', { origin: 'button' })
